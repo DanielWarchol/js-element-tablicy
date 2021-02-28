bdb90489a7e796d7a05b15e80fd96031482df0cc
DanielsKilLs20 <64350122+DanielsKilLs20@users.noreply.github.com>
2021-02-28 11:56:15 +0100

    Update main.js

--- js/main.js
+++ js/main.js
@@ -2,9 +2,11 @@ let tablica = [10 , 50 , 32 , 1 , 100 ];
    
 
 
-function number(){
+function arrayElement(tablica){
 
   return tablica[0];
 
 }
-console.log(number([tablica]));
+console.log(arrayElement(tablica));
+
+console.log(arrayElement([39, 20 , 900 , 4 , 16]));
